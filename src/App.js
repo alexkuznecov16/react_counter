63e4d4e14474d6715cd10f752ccfff7c19d7dd73
Alexander Kuznecov <alexander.kuznecov16@gmail.com>
2023-03-02 15:22:25 +0200

    all ready

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
+import React from 'react';
+import './main.scss';
+
 function App() {
+
+  //* Создаём [переменную, меняющиеся при использовании функции] = (начальное число)
+  let [count, getCount] = React.useState(0);
+
+  //* По клику функция getCount прибавляет 1 к числу
+  const onClickPlus = () => {
+    getCount(count+1);
+  }
+
+  //* По клику функция getCount отнимает 1 у числа
+  const onClickMinus = () => {
+    getCount(count-1);
+  }
+
+  //* По клику функция getCount сбрасывается до нуля
+  const onClickReset = () => {
+    getCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="counter">
+        <div className="counter__inner">
+          <h2 className="counter__name">Counter:</h2>
+          <h1 className="counter__num">{count}</h1>
+          <div className="counter__buttons">
+            <button onClick={onClickMinus} className='minus'>-</button>
+            <button onClick={onClickReset} className="reset">Reset</button>
+            <button onClick={onClickPlus} className='plus'>+</button>
+          </div>
+        </div>
+      </div>
+      <footer>
+        &copy; Alexander Kuznecov 2023
+      </footer>
+    </body>
   );
 }
 
